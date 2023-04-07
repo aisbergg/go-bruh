8629709b4a64fd8224f546eb8b4a240b9ac8c963
Andre Lehmann <aisberg@posteo.de>
2023-04-08 01:49:03 +0200

    test: add benchmark

---
+++ benchmark/bench_test.go
@@ -0,0 +1,189 @@
+package bench_test
+
+import (
+	goerrors "errors"
+	"fmt"
+	"testing"
+
+	"github.com/aisbergg/go-bruh/pkg/bruh"
+	pkgerrors "github.com/pkg/errors"
+	"github.com/rotisserie/eris"
+)
+
+var (
+	global interface{}
+	cases  = []struct {
+		layers int
+	}{
+		{1},
+		{10},
+		{100},
+	}
+)
+
+func wrapStdErrors(layers int) error {
+	err := goerrors.New("std errors")
+	for i := 0; i < layers; i++ {
+		err = fmt.Errorf("wrap %v: %w", i, err)
+	}
+	return err
+}
+
+func wrapPkgErrors(layers int) error {
+	err := pkgerrors.New("std errors")
+	for i := 0; i < layers; i++ {
+		err = pkgerrors.Wrapf(err, "wrap %v", i)
+	}
+	return err
+}
+
+func wrapEris(layers int) error {
+	err := eris.New("std errors")
+	for i := 0; i < layers; i++ {
+		err = eris.Wrapf(err, "wrap %v", i)
+	}
+	return err
+}
+
+func wrapBruh(layers int) error {
+	err := bruh.New("std errors")
+	for i := 0; i < layers; i++ {
+		err = bruh.Wrapf(err, "wrap %v", i)
+	}
+	return err
+}
+
+func BenchmarkWrap(b *testing.B) {
+	for _, tc := range cases {
+		b.Run(fmt.Sprintf("std errors %v layers", tc.layers), func(b *testing.B) {
+			var err error
+			for n := 0; n < b.N; n++ {
+				err = wrapStdErrors(tc.layers)
+			}
+			b.StopTimer()
+			global = err
+		})
+
+		b.Run(fmt.Sprintf("pkg errors %v layers", tc.layers), func(b *testing.B) {
+			var err error
+			for n := 0; n < b.N; n++ {
+				err = wrapPkgErrors(tc.layers)
+			}
+			b.StopTimer()
+			global = err
+		})
+
+		b.Run(fmt.Sprintf("eris %v layers", tc.layers), func(b *testing.B) {
+			var err error
+			for n := 0; n < b.N; n++ {
+				err = wrapEris(tc.layers)
+			}
+			b.StopTimer()
+			global = err
+		})
+
+		b.Run(fmt.Sprintf("bruh %v layers", tc.layers), func(b *testing.B) {
+			var err error
+			for n := 0; n < b.N; n++ {
+				err = wrapBruh(tc.layers)
+			}
+			b.StopTimer()
+			global = err
+		})
+	}
+}
+
+func BenchmarkFormatWithoutTrace(b *testing.B) {
+	for _, tc := range cases {
+		b.Run(fmt.Sprintf("std errors %v layers", tc.layers), func(b *testing.B) {
+			err := wrapStdErrors(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = fmt.Sprint(err)
+			}
+			b.StopTimer()
+			global = str
+		})
+
+		b.Run(fmt.Sprintf("pkg errors %v layers", tc.layers), func(b *testing.B) {
+			err := wrapPkgErrors(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = fmt.Sprint(err)
+			}
+			b.StopTimer()
+			global = str
+		})
+
+		b.Run(fmt.Sprintf("eris %v layers", tc.layers), func(b *testing.B) {
+			err := wrapEris(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = eris.ToString(err, false)
+			}
+			b.StopTimer()
+			global = str
+		})
+
+		b.Run(fmt.Sprintf("bruh %v layers", tc.layers), func(b *testing.B) {
+			err := wrapBruh(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = bruh.ToString(err, false)
+			}
+			b.StopTimer()
+			global = str
+		})
+	}
+}
+
+func BenchmarkFormatWithTrace(b *testing.B) {
+	for _, tc := range cases {
+		b.Run(fmt.Sprintf("pkg errors %v layers", tc.layers), func(b *testing.B) {
+			err := wrapPkgErrors(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = fmt.Sprintf("%+v", err)
+			}
+			b.StopTimer()
+			global = str
+		})
+
+		b.Run(fmt.Sprintf("eris %v layers", tc.layers), func(b *testing.B) {
+			err := wrapEris(tc.layers)
+			format := eris.StringFormat{
+				Options: eris.FormatOptions{
+					WithTrace:    true,
+					WithExternal: true,
+				},
+				MsgStackSep:  "\n",
+				PreStackSep:  "    ",
+				StackElemSep: ":",
+				ErrorSep:     "\n",
+			}
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = eris.ToCustomString(err, format)
+			}
+			b.StopTimer()
+			global = str
+		})
+
+		b.Run(fmt.Sprintf("bruh %v layers", tc.layers), func(b *testing.B) {
+			err := wrapBruh(tc.layers)
+			b.ResetTimer()
+			var str string
+			for n := 0; n < b.N; n++ {
+				str = bruh.ToString(err, true)
+			}
+			b.StopTimer()
+			global = str
+		})
+	}
+}
---
+++ benchmark/go.mod
@@ -0,0 +1,11 @@
+module github.com/aisbergg/go-bruh/benchmark
+
+go 1.18
+
+require (
+	github.com/aisbergg/go-bruh v0.9.0
+	github.com/pkg/errors v0.9.1
+	github.com/rotisserie/eris v0.5.4
+)
+
+replace github.com/aisbergg/go-bruh => ./../
